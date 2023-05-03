82de97052cf085094412b8db281e72c82a407dba
shaiunterslak <40061080+shaiunterslak@users.noreply.github.com>
2023-05-03 14:06:23 -0500

    add capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.reactispotify.reactspotify',
+  appName: 'react-spotify',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
